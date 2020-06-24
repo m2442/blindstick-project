db3c4defef6850b71c7402407c115ab136a456ef
manimay raj <manimayraj24@gmail.com>
2020-06-24 06:23:21 +0530

    publishinh now

--- gallary/views.py
+++ gallary/views.py
@@ -4,3 +4,9 @@ from .models import Gallary
 def home(request):
     pics = Gallary.objects
     return render(request, 'gallary/home.html',{'pics':pics})
+
+def about(request):
+    return render(request, 'gallary/about.html')
+
+def starttracking(request):
+    return render(request, 'gallary/starttracking.html')
